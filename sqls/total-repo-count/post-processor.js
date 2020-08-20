9a5aa1fa1634aa6acf537af87224f2ae87a7e7f5
Frankzhaopku <syzhao1988@126.com>
2020-08-20 20:12:22 +0800

    refactor: change to old way
    Signed-off-by: Frankzhaopku <syzhao1988@126.com>

--- sqls/total-repo-count/post-processor.js
+++ sqls/total-repo-count/post-processor.js
@@ -1,5 +1,5 @@
 module.exports = async function(data) {
-  if (!Number.isInteger(data[0].count)) {
+  if (!data[0].count || Number.isNaN(parseInt(data[0].count))) {
     throw new Error('Invalid data');
   }
   return data[0].count;
