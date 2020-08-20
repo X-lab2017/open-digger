bcdd22216373ee6950db944f4d5b435e56fe3c84
Frankzhaopku <syzhao1988@126.com>
2020-08-20 12:16:46 +0800

    feat:add-sql-comp
    Signed-off-by: Frankzhaopku <syzhao1988@126.com>

---
+++ .github/hypertrons-components/sql-verified/config.ts
@@ -0,0 +1,21 @@
+import { configClass, configProp } from '../decorators';
+import defaultConfig from './defaultConfig';
+
+@configClass({
+  description: 'Sql verified command config',
+})
+export default class Config {
+
+  @configProp({
+    description: 'Label name',
+    defaultValue: defaultConfig.label,
+  })
+  label: string;
+
+  @configProp({
+    description: 'Command name',
+    defaultValue: defaultConfig.command,
+  })
+  command: string;
+
+}
---
+++ .github/hypertrons-components/sql-verified/defaultConfig.ts
@@ -0,0 +1,8 @@
+import Config from './config';
+
+const defaultConfig: Config = {
+  label: 'pull/sql-verified',
+  command: '/sql-verified',
+};
+
+export default defaultConfig;
