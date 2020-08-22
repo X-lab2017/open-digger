87affbe42ead138dea8255eb6f23df2183a8ce1f
Frankzhaopku <syzhao1988@126.com>
2020-08-22 17:47:19 +0800

    feat: add sql run component for hypertrons
    Signed-off-by: Frankzhaopku <syzhao1988@126.com>

---
+++ .github/hypertrons-components/sql_run/defaultConfig.ts
@@ -0,0 +1,40 @@
+// Copyright 2019 - present Xlab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Config from './config';
+
+const defaultConfig: Config = {
+  sqlRequestUrl: 'http://localhost:7001/query',
+  command: '/sql-run',
+  sqlFileRegex: 'sqls/([%w|-]+)/sql',
+  manifestFileRegex: 'sqls/([%w|-]+)/manifest%.json',
+  postProcessFileRegex: 'sqls/([%w|-]+)/post%-processor%.js',
+  defaultRenderParams: {
+    year: 2020,
+    table: 'github_log.year2020',
+  },
+  commentItemTemplate: `I found SQL component \`{{sqlName}}\` in this PR, the SQL run result data is:
+\`\`\`
+{{data}}
+\`\`\`
+The renderred text is:
+
+{{text}}
+
+Please check whether the result is as expected.
+
+`,
+};
+
+export default defaultConfig;
