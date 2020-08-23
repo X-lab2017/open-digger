4665a92957eb8b32592b5a130c2c991525e9f72f
Frankzhaopku <syzhao1988@126.com>
2020-08-24 00:19:48 +0800

    sql: to thousands
    Signed-off-by: Frankzhaopku <syzhao1988@126.com>

--- sqls/total-record-count/post-processor.js
+++ sqls/total-record-count/post-processor.js
@@ -2,5 +2,8 @@ module.exports = async function(data) {
   if (!data[0].count || Number.isNaN(parseInt(data[0].count))) {
     throw new Error('Invalid data');
   }
-  return data[0].count;
+  var toThousands = (num) => {
+    return (num || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
+  }
+  return toThousands(data[0].count);
 }
