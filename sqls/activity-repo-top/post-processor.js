483b564b93a365b13bd8e94a93311f4a27eddecb
Haoyue <wsunshine1089@163.com>
2020-10-26 23:09:54 +0800

    Docs: Add repo activity top 10 into the report template

--- sqls/activity-repo-top/post-processor.js
+++ sqls/activity-repo-top/post-processor.js
@@ -1,8 +1,8 @@
 module.exports = async function(data) {
-  let ret = '| # | repo_id | repo_activity |\n';
-  ret += '|:--:|:--:|:--:|\n';
+  let ret = '| # | repo_id | repo_name | repo_activity |\n';
+  ret += '|:--:|:--:|:--:|:--:|\n';
   data.forEach((item, index) => {
-    ret += `| ${index + 1} | ${item.repo_id} | ${item.repo_activity} |\n`;
+    ret += `| ${index + 1} | ${item.repo_id} | ${item.repo_name} | ${item.repo_activity} |\n`;
   });
   return ret;
 }
