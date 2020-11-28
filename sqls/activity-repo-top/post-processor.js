2bfc4f94a3e2498ec4603d0c2f4511665a1190b1
sunshinemingo <37278844+sunshinemingo@users.noreply.github.com>
2020-11-28 11:55:01 +0800

    refactor:Add more details for top repos (#213)

--- sqls/activity-repo-top/post-processor.js
+++ sqls/activity-repo-top/post-processor.js
@@ -1,8 +1,8 @@
 module.exports = async function(data) {
-  let ret = '| # | repo_id | repo_name | repo_activity |\n';
-  ret += '|:--:|:--:|:--:|:--:|\n';
+  let ret = '| # | repo_id | repo_name | repo_activity | developer_count | open_issue | issue_comment | open_pull | pull_review_comment | merge_pull |\n';
+  ret += '|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|\n';
   data.forEach((item, index) => {
-    ret += `| ${index + 1} | ${item.repo_id} | ${item.repo_name} | ${item.repo_activity} |\n`;
+    ret += `| ${index + 1} | ${item.repo_id} | ${item.repo_name} | ${item.repo_activity} | ${item.actor_count} | ${item.oic_count} | ${item.icc_count} | ${item.opc_count} | ${item.rcc_count} | ${item.mpc_count} |\n`;
   });
   return ret;
 }
