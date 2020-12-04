ad1ab63143f3b52c8e10476fa5d4403d832c50b7
zaki <weng_zhen_jie@163.com>
2020-12-04 10:14:56 +0800

    changes for top repos and develop

--- sqls/activity-repo-top/post-processor.js
+++ sqls/activity-repo-top/post-processor.js
@@ -1,8 +1,8 @@
 module.exports = async function(data) {
-  let ret = '| # | repo_id | repo_name | repo_activity | developer_count | open_issue | issue_comment | open_pull | pull_review_comment | merge_pull |\n';
-  ret += '|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|\n';
+  let ret = '| # | name | language | activity | developer_count | open_issue | issue_comment | open_pull | pull_review_comment | merge_pull | pull_commits | pull_additions | pull_deletions |\n';
+  ret += '|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|\n';
   data.forEach((item, index) => {
-    ret += `| ${index + 1} | ${item.repo_id} | ${item.repo_name} | ${item.repo_activity} | ${item.actor_count} | ${item.oic_count} | ${item.icc_count} | ${item.opc_count} | ${item.rcc_count} | ${item.mpc_count} |\n`;
+    ret += `| ${index + 1} | ${item.repo_name} | ${item.repo_language} | ${item.repo_activity} | ${item.developer_count} | ${item.open_issue} | ${item.issue_comment} | ${item.open_pull} | ${item.pull_review_comment} | ${item.merge_pull} | ${item.commits} | ${item.additions} | ${item.deletions} |\n`;
   });
   return ret;
 }
