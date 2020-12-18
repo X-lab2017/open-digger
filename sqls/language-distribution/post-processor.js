f5eb64cdb83a31d6dcbed8e33a2b2d4e8e66c76a
Xiaoya <xiaoyaxiaesther@gmail.com>
2020-12-18 15:46:24 +0800

    Update post-processor.js
    Signed-off-by: Xiaoya <xiaoyaxiaesther@gmail.com>

--- sqls/language-distribution/post-processor.js
+++ sqls/language-distribution/post-processor.js
@@ -1,8 +1,8 @@
 module.exports = async function(data) {
-  let ret = '| # | language | count | top_repo | activity | \n';
+  let ret = '| # | repo_language | count | top_repo | activity | \n';
   ret += '|:--:|:--:|:--:|:--:|:--:| \n';
   data.forEach((item, index) => {
-    ret += `| ${index + 1} | ${item.language} | ${item.count} | ${item.top_repo} | ${item.activity} | \n`;
+    ret += `| ${index + 1} | ${item.repo_language} | ${item.count} | ${item.top_repo} | ${item.activity} | \n`;
   });
   return ret;
 }
