36ec89c747ad12319d05bae8763d37f6beee4e69
Frankzhaopku <syzhao1988@126.com>
2021-01-07 14:55:22 +0800

    fix: minor changes to components
    Signed-off-by: Frankzhaopku <syzhao1988@126.com>

--- case-study/sqls/top-developer-by-repo/post-processor.js
+++ case-study/sqls/top-developer-by-repo/post-processor.js
@@ -10,13 +10,13 @@ module.exports = async function(data, param, config) {
   let ret = '';
   for (let i in repoData) {
     let d = repoData[i];
-    ret += `- ${d.repo_name}\n
-| # | login | activity | issue_comment | open_issue | open_pull | pull_review_comment | merge_pull | pull_commits | pull_additions | pull_deletions |\n|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|\n`;
+    ret += `\n- ${i}\n
+| # | login | activity | issue_comment | open_issue | open_pull | pull_review_comment | merge_pull | pull_commits | pull_additions | pull_deletions |\n|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|\n`;
     d.sort((a, b) => {
-      return b.develpoer_activity - a.develpoer_activity;
+      return b.developer_activity - a.developer_activity;
     });
     d.slice(0, config.topN).forEach((item, index) => {
-      ret += `| ${index + 1} | ${item.actor_login} | ${item.develpoer_activity} |  ${item.issue_comment} | ${item.open_issue} | ${item.open_pull} | ${item.pull_review_comment} | ${item.merge_pull} | ${item.pull_commits} | ${item.pull_additions} | ${item.pull_deletions} |\n`;
+      ret += `| ${index + 1} | ${item.actor_login} | ${item.developer_activity} |  ${item.issue_comment} | ${item.open_issue} | ${item.open_pull} | ${item.pull_review_comment} | ${item.merge_pull} | ${item.pull_commits} | ${item.pull_additions} | ${item.pull_deletions} |\n`;
     });
   }
   return ret;
