3390ac000e7a4a3208ae4bb665051693a8a1e1a6
longyanz <91717658+longyanz@users.noreply.github.com>
2022-08-03 20:59:22 +0800

    Update open_digger.js

--- src/open_digger.js
+++ src/open_digger.js
@@ -36,6 +36,7 @@ const openDigger = {
       busFactor: func.chaossBusFactor,
       changeRequestsAccepted: func.chaossChangeRequestsAccepted,
       changeRequestsDeclined: func.chaossChangeRequestsDeclined,
+      changeRequestsAcceptedRatio: func.chaossChangeRequestsAcceptedRatio,
     }
   },
   relation: {
