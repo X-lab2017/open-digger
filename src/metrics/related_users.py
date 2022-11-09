3383cc27d6396a5caef522d1b47a716d9847cd20
xgdyp <zxyasrf@163.com>
2022-11-09 11:23:12 +0800

    fix: delete python files in src folder

--- src/metrics/related_users.py
+++
@@ -1,9 +0,0 @@
-from basic import getMergedConfig, getRepoWhereClauseForNeo4j, getTimeRangeWhereClauseForNeo4j
-import db.neo4j_driver as neo4j_driver
-
-def getRelatedUsers(config):
-    config = getMergedConfig(config)
-    repoWhereClause = getRepoWhereClauseForNeo4j(config)
-    timeWhereClause = getTimeRangeWhereClauseForNeo4j(config, 'a')
-    query = 'MATCH (r:Repo)<-[a:ACTION]-(u:User) WHERE {} {} RETURN DISTINCT u.login AS user_login {};'.format(repoWhereClause + ' AND ' if repoWhereClause != None else '', timeWhereClause, 'LIMIT {}'.format(config.get('limit')) if config.get('limit') > 0 else '')
-    return neo4j_driver.query(query)
