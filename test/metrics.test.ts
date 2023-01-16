ce8265c5cb553f2a7fcf2466a4394eb1903ef3fc
frank-zsy <syzhao1988@126.com>
2023-01-16 16:28:44 +0800

    feat: add order option
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- test/metrics.test.ts
+++ test/metrics.test.ts
@@ -3,18 +3,39 @@ const openDigger = require('../src/metrics/index');
 
 describe('Index and metric test', () => {
   // use repos under google org in 2015 to 2016
-  const limit = 3;
-  const option = {
-    startYear: 2015, endYear: 2016, startMonth: 1, endMonth: 12,
-    orgIds: [1342004], order: 'DESC', limit,
-    groupTimeRange: 'month',
-  };
-  const months = 24;
-  const commonAssert = async (func: (option: any) => Promise<any[]>, key: string) => {
-    const result = await func(option);
-    assert.strictEqual(result.length, limit); // the limit works fine
+  const commonAssert = async (func: (option: any) => Promise<any[]>, key: string, index?: number) => {
+    const years = 2;
+    const months = 24;
+
+    // year group with latest order
+    const option: any = {
+      startYear: 2015, endYear: 2016, startMonth: 1, endMonth: 12,
+      orgIds: [1342004], order: 'DESC', limit: 3,
+      groupTimeRange: 'year',
+    };
+    let result = await func(option);
+    assert.strictEqual(result.length, option.limit); // the limit works fine
+    assert.strictEqual(result.every(r => r[key].length === years), true);  // the main field has correct length
+    // order works fine
+    if (index !== undefined) {
+      assert.strictEqual(parseFloat(result[0][key][years - 1][index]) >= parseFloat(result[option.limit - 1][key][years - 1][index]), true);
+    } else {
+      assert.strictEqual(parseFloat(result[0][key][years - 1]) >= parseFloat(result[option.limit - 1][key][years - 1]), true);
+    }
+
+    // month group with total order
+    option.groupTimeRange = 'month';
+    option.orderOption = 'total';
+    result = await func(option);
+    assert.strictEqual(result.length, option.limit); // the limit works fine
     assert.strictEqual(result.every(r => r[key].length === months), true);  // the main field has correct length
-    assert.strictEqual(parseFloat(result[0][key][months - 1]) >= parseFloat(result[2][key][months - 1]), true); // order works fine
+    // order works fine
+    const sumFunc = (res: any, f?: any): number => res[key].map(r => parseFloat(f ? f(r) : r)).reduce((p, c) => p + c);
+    if (index !== undefined) {
+      assert.strictEqual(sumFunc(result[0], r => r[index]) >= sumFunc(result[option.limit - 1], r => r[index]), true);
+    } else {
+      assert.strictEqual(sumFunc(result[0]) >= sumFunc(result[option.limit - 1]), true);
+    }
   };
   describe('Indices tests', () => {
     it('activity', async () => {
@@ -55,7 +76,7 @@ describe('Index and metric test', () => {
       await commonAssert(openDigger.chaossIssueResolutionDuration, 'resolution_duration');
     });
     it('issue response time', async () => {
-      await commonAssert(openDigger.chaossIssueResponseTime, 'issue_response_time');
+      await commonAssert(openDigger.chaossIssueResponseTime, 'issue_response_time', 1);
     });
     it('code change lines', async () => {
       await commonAssert(openDigger.chaossCodeChangeLines, 'lines');
