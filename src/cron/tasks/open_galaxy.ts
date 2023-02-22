4d4af8726b5efcfe6a7b65e174af5d8f8b39335b
frank-zsy <syzhao1988@126.com>
2023-02-22 16:55:32 +0800

    feat: add continuous 3d layout support
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- src/cron/tasks/open_galaxy.ts
+++ src/cron/tasks/open_galaxy.ts
@@ -8,6 +8,12 @@ import { existsSync, mkdirSync, readFileSync, rmSync, writeFileSync } from "fs";
 var createLayout = require('ngraph.offline.layout');
 var save = require('ngraph.tobinary');
 
+interface Node {
+  id: string; // id of the node
+  t: string;  // type of the node
+  n: string;  // name of the node
+  or: Number; // OpenRank of the node
+};
 /**
  * This task is used to generate data for open galaxy
  * https://github.com/X-lab2017/open-galaxy
@@ -17,64 +23,95 @@ const task: Task = {
   enable: true,
   immediate: true,
   callback: async () => {
-    const forceClear = true;
+    const forceClear = false;
 
     const config = await getConfig();
     const exportBasePath = config.export.galaxyPath;
     const lastMonth = new Date();
     lastMonth.setMonth(lastMonth.getMonth() - 1);
+
+    const labelsFileName = 'labels.json', originLabelsFileName = 'labels_origin.json', linksFileName = 'links.bin', positionsFileName = 'positions.bin';
+
+    const lastPositionMap = new Map<string, Number[]>();
+
     await forEveryMonth(2015, 1, lastMonth.getFullYear(), lastMonth.getMonth() + 1, async (y, m) => {
       console.log(`Gonna export ${y}-${m}`);
-      const nodeMap = new Map<string, any>();
-      const edgeArr: any[] = [];
-      await runQueryStream(`MATCH (r:Repo)<-[a:ACTION]-(u:User) WHERE r.open_rank_${y}${m} > e() AND u.open_rank_${y}${m} > e() AND a.activity_${y}${m} > 2 RETURN id(r) AS rid, r.name AS repoName, ROUND(r.open_rank_${y}${m}, 2) AS ror, id(u) AS uid, u.login AS userName, ROUND(u.open_rank_${y}${m}, 2) AS uor, a.activity_${y}${m} AS activity`, async r => {
-        nodeMap.set(r.rid, { t: 'r', n: r.repoName, or: r.ror });
-        nodeMap.set(r.uid, { t: 'u', n: r.userName, or: r.uor });
-        edgeArr.push({ from: r.uid, to: r.rid, weight: r.activity });
-      });
-      console.log(`Load graph done, node size is ${nodeMap.size}, edge size is ${edgeArr.length}`);
-      const g = createGraph();
-      for (const n of nodeMap.entries()) {
-        g.addNode(n[0], n[1]);
-      }
-      var maxWeight = 0;
-      for (const e of edgeArr) {
-        const link: any = g.addLink(e.from, e.to);
-        link.length = e.weight;
-        if (e.weight > maxWeight) maxWeight = e.weight;
-      }
-      // adjust link length
-      const maxLength = 30, minLength = 10;
-      g.forEachLink(link => {
-        (link as any).length = maxLength - (((link as any).length / maxWeight) * (maxLength - minLength) + minLength) + minLength;
-      });
 
-      const exportPath = join(exportBasePath, `${y}-${m}`);
+      // save the graph
+      const exportPath = join(exportBasePath, `${y}${m.toString().padStart(2, '0')}`);
       if (forceClear && existsSync(exportPath)) {
         rmSync(exportPath, { recursive: true });
       }
       if (!existsSync(exportPath)) {
         mkdirSync(exportPath, { recursive: true });
       }
-      save(g, {
-        outDir: exportPath,
-        labels: 'labels_origin.json',
-      });
 
-      const nodeIds = JSON.parse(readFileSync(join(exportPath, 'labels_origin.json')).toString());
-      writeFileSync(join(exportPath, 'labels.json'), JSON.stringify(nodeIds.map(id => nodeMap.get(id))));
-      rmSync(join(exportPath, 'labels_origin.json'));
+      // generate the graph if files not exists or deleted
+      if (![labelsFileName, linksFileName, positionsFileName].every(f => existsSync(join(exportPath, f)))) {
+        const nodeMap = new Map<string, Node>();
+        const edgeArr: any[] = [];
 
-      const layout = createLayout(g, {
-        iterations: 3000,
-        saveEach: 3000,
-        timeStep: 2,
-        outDir: exportPath,
-        layoutOptions: {
-          nodeMass: id => g.getNode(id)?.data.or,
+        // export nodes and edges
+        await runQueryStream(`MATCH (r:Repo)<-[a:ACTION]-(u:User) WHERE r.open_rank_${y}${m} > e() AND u.open_rank_${y}${m} > e() AND a.activity_${y}${m} > 2 RETURN id(r) AS rid, r.name AS repoName, ROUND(r.open_rank_${y}${m}, 2) AS ror, id(u) AS uid, u.login AS userName, ROUND(u.open_rank_${y}${m}, 2) AS uor, a.activity_${y}${m} AS activity`, async r => {
+          nodeMap.set(r.rid, { id: r.rid, t: 'r', n: r.repoName, or: r.ror });
+          nodeMap.set(r.uid, { id: r.uid, t: 'u', n: r.userName, or: r.uor });
+          edgeArr.push({ from: r.uid, to: r.rid, weight: r.activity });
+        });
+        console.log(`Load graph done, node size is ${nodeMap.size}, edge size is ${edgeArr.length}`);
+
+        // build graph in ngraph
+        const g = createGraph();
+        for (const n of nodeMap.entries()) {
+          g.addNode(n[0], n[1]);
         }
+        var maxWeight = 0;
+        for (const e of edgeArr) {
+          const link: any = g.addLink(e.from, e.to);
+          link.length = e.weight;
+          if (e.weight > maxWeight) maxWeight = e.weight;
+        }
+        // adjust link length
+        const maxLength = 30, minLength = 10;
+        g.forEachLink(link => {
+          (link as any).length = maxLength - (((link as any).length / maxWeight) * (maxLength - minLength) + minLength) + minLength;
+        });
+        save(g, {
+          outDir: exportPath,
+          labels: originLabelsFileName,
+        });
+
+        // regenerate label file
+        const nodeIds = JSON.parse(readFileSync(join(exportPath, originLabelsFileName)).toString());
+        writeFileSync(join(exportPath, labelsFileName), JSON.stringify(nodeIds.map(id => nodeMap.get(id))));
+        rmSync(join(exportPath, originLabelsFileName));
+
+        // create layout
+        const layout = createLayout(g, {
+          iterations: 1000,
+          saveEach: 1000,
+          outDir: exportPath,
+          layoutOptions: {
+            nodeMass: id => g.getNode(id)?.data.or,
+          }
+        });
+        // set node position to last month positions
+        nodeIds.forEach(id => {
+          if (lastPositionMap.has(id)) {
+            layout.getLayout().setNodePosition(id, ...lastPositionMap.get(id)!);
+          }
+        });
+        layout.run(true);
+      }
+
+      // load the position info for next month
+      const nodes = JSON.parse(readFileSync(join(exportPath, labelsFileName)).toString());
+      const buf = readFileSync(join(exportPath, positionsFileName));
+      nodes.forEach((node, index) => {
+        const offset = index * 4 * 3;
+        const coordinates = [0, 4, 8].map(i => buf.readInt32LE(offset + i));
+        lastPositionMap.set(node.id, coordinates);
       });
-      layout.run(true);
+      console.log(`Find ${lastPositionMap.size} nodes' positions from ${y}-${m}`);
     });
   },
 };
