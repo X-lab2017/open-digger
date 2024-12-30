6fc6878e76f04f221d89ae1a591e9a9be7b6f8e2
frank-zsy <syzhao1988@126.com>
2024-12-30 22:29:58 +0800

    fix: fix docker image tag and tsconfig target
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- sample_data/build/build_image.sh
+++ sample_data/build/build_image.sh
@@ -1,2 +1,2 @@
 docker image rm open-digger-clickhouse-base:v2
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag open-digger-docker-registry.cn-beijing.cr.aliyuncs.com/open-digger/open-digger-clickhouse-base:v2 .
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag registry.cn-beijing.aliyuncs.com/open-digger/open-digger-clickhouse-base:v2 .
