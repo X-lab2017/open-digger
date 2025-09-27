20f9455e16db805aaa018552b96a36fc3fc40212
Frank Zhao <syzhao1988@126.com>
2025-09-28 00:03:47 +0800

    feat: add view of user info and add chinese province data (#1720)
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- src/scripts/importLabelToDatabase.ts
+++ src/scripts/importLabelToDatabase.ts
@@ -8,7 +8,7 @@ import { query, insertRecords } from "../db/clickhouse";
       id String,
       type LowCardinality(String),
       name String,
-      name_zh Nullable(String),
+      name_zh String,
       children Array(String),
       platforms Nested(
         name LowCardinality(String),
@@ -17,7 +17,7 @@ import { query, insertRecords } from "../db/clickhouse";
         repos Array(UInt64),
         users Array(UInt64),
       ),
-      data Nullable(String),
+      data String,
     ) ENGINE = ReplacingMergeTree ORDER BY id`);
   };
   const labelData = getLabelData();
@@ -27,14 +27,14 @@ import { query, insertRecords } from "../db/clickhouse";
     id: label.identifier,
     type: label.type,
     name: label.name,
-    name_zh: label.name_zh ?? null,
+    name_zh: label.name_zh ?? '',
     children: label.children,
     'platforms.name': label.platforms.map(platform => platform.name),
     'platforms.type': label.platforms.map(platform => platform.type),
     'platforms.orgs': label.platforms.map(platform => platform.orgs.map(org => org.id)),
     'platforms.repos': label.platforms.map(platform => platform.repos.map(repo => repo.id)),
     'platforms.users': label.platforms.map(platform => platform.users.map(user => user.id)),
-    data: label.meta ? JSON.stringify(label.meta) : null,
+    data: label.meta ? JSON.stringify(label.meta) : '',
   }));
 
   await insertRecords(records, 'labels');
