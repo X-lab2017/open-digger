15e875b3062c7a623b25d0072e584ddad740197d
frank-zsy <syzhao1988@126.com>
2026-06-05 10:42:52 +0800

    label: add more bot label
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- src/scripts/completeLabel.ts
+++ src/scripts/completeLabel.ts
@@ -44,54 +44,62 @@ async function readPath(p: string, base: string, fileProcessor: (f: string) => P
     if (!f.endsWith(labelFileSuffix)) return;
     const filePath = path.join(labelInputPath, f);
     const label = readFileAsObj(filePath);
-    try {
-      for (const p of label.data?.platforms ?? []) {
-        if (p.repos) {
-          for (const r of p.repos) {
-            if (!r.id) {
+    for (const p of label.data?.platforms ?? []) {
+      if (p.repos) {
+        for (const r of p.repos) {
+          if (!r.id) {
+            try {
               if (p.name !== 'GitHub') {
                 throw new Error('Only GitHub supported now.');
               }
               const data = await oct.repos.get({ owner: r.name.split('/')[0], repo: r.name.split('/')[1] });
               r.id = data.data.id;
               console.log(`Get repo id of ${r.name} for ${p.name}: ${r.id}`);
+            } catch (e: any) {
+              console.log(`Error processing repo in ${f}, e=${e.message}, repo=${JSON.stringify(r)}`);
             }
           }
         }
-        if (p.orgs) {
-          for (const o of p.orgs) {
-            if (!o.id) {
+      }
+      if (p.orgs) {
+        for (const o of p.orgs) {
+          if (!o.id) {
+            try {
               if (p.name !== 'GitHub') {
                 throw new Error('Only GitHub supported now.');
               }
               const data = await oct.orgs.get({ org: o.name });
               o.id = data.data.id;
               console.log(`Get org id of ${o.name} for ${p.name}: ${o.id}`);
+            } catch (e: any) {
+              console.log(`Error processing org in ${f}, e=${e.message}, org=${JSON.stringify(o)}`);
             }
           }
         }
-        if (p.users) {
-          for (const u of p.users) {
-            if (!u.id) {
+      }
+      if (p.users) {
+        for (const u of p.users) {
+          if (!u.id) {
+            try {
               if (p.name !== 'GitHub') {
                 throw new Error('Only GitHub supported now.');
               }
               const data = await oct.users.getByUsername({ username: u.name });
               u.id = data.data.id;
-              console.log(`Get repo id of ${u.name} for ${p.name}: ${u.id}`);
+              console.log(`Get user id of ${u.name} for ${p.name}: ${u.id}`);
+            } catch (e: any) {
+              console.log(`Error processing user in ${f}, e=${e.message}, user=${JSON.stringify(u)}`);
             }
           }
         }
-        normalizePlatformEntities(p.repos);
-        normalizePlatformEntities(p.orgs);
-        normalizePlatformEntities(p.users);
       }
-      if (label.data && label.data.labels) {
-        label.data.labels = label.data.labels.sort();
-      }
-      writeFileSync(filePath, dump(label, { noRefs: true, lineWidth: -1 }));
-    } catch (e: any) {
-      console.log(`Error processing ${f}, e=${e.message}, label=${JSON.stringify(label)}`);
+      normalizePlatformEntities(p.repos);
+      normalizePlatformEntities(p.orgs);
+      normalizePlatformEntities(p.users);
     }
+    if (label.data && label.data.labels) {
+      label.data.labels = label.data.labels.sort();
+    }
+    writeFileSync(filePath, dump(label, { noRefs: true, lineWidth: -1 }));
   });
 })();
